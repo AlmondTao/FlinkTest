1b1219d84a47bdac4df35141ff98ef8ae733b930
TQY <taoqingyang@gome.com.cn>
2023-08-21 17:19:05 +0800

    自定义wal

--- src/main/java/com/tqy/checkpoint/CheckpointDemo2.java
+++ src/main/java/com/tqy/checkpoint/CheckpointDemo2.java
@@ -1,5 +1,7 @@
 package com.tqy.checkpoint;
 
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.api.java.typeutils.runtime.PojoSerializer;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
@@ -7,21 +9,33 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.runtime.operators.CheckpointCommitter;
 import org.apache.flink.streaming.runtime.operators.GenericWriteAheadSink;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Calendar;
 import java.util.Random;
+import java.util.UUID;
 
 //WAL机制
 public class CheckpointDemo2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        ExecutionConfig config = env.getConfig();
+
+        TypeSerializer<Event> serializer = TypeInformation.of(Event.class).createSerializer(config);
         env.setParallelism(1);
         env.setStateBackend(new HashMapStateBackend());
-        env.enableCheckpointing(10*1000L);
+        env.enableCheckpointing(5*1000L);
         env.getCheckpointConfig().setCheckpointStorage("file:///FlinkTest/src/main/checkpoint");
 
         env
-                .addSource(new ClickSource());
+                .addSource(new ClickSource(-1))
+                .transform("MyWALSink",TypeInformation.of(Event.class),new MyWALSink(new MyCheckpointCommitter(),serializer, UUID.randomUUID().toString()));
+
+        env.execute();
+
+
+
 
     }
 
@@ -34,7 +48,12 @@ public class CheckpointDemo2 {
 
         @Override
         protected boolean sendValues(Iterable<Event> values, long checkpointId, long timestamp) throws Exception {
-            return false;
+            System.out.println("检查点:"+checkpointId+" 开始发送数据:");
+            for (Event event : values){
+                System.out.println(event);
+            }
+            System.out.println("数据发送完毕");
+            return true;
         }
     }
 
@@ -61,13 +80,25 @@ public class CheckpointDemo2 {
         public void commitCheckpoint(int subtaskIdx, long checkpointID) throws Exception {
             this.subtaskIdx = subtaskIdx;
             this.checkpointID = checkpointID;
-            this.committed = true;
-            System.out.println("checkPointCommitter commitCheckpoint");
+            if (this.subtaskIdx == subtaskIdx && this.checkpointID == checkpointID){
+                this.committed = true;
+                System.out.println("checkPointCommitter commitCheckpoint,subtaskIdx:"+subtaskIdx+" checkpointID:"+checkpointID);
+            }
+
+
         }
 
         @Override
         public boolean isCheckpointCommitted(int subtaskIdx, long checkpointID) throws Exception {
-            return this.committed;
+            System.out.println("checkPointCommitter isCheckpointCommitted,subtaskIdx:"+subtaskIdx+" checkpointID:"+checkpointID);
+            if (this.subtaskIdx == subtaskIdx && this.checkpointID == checkpointID && this.committed == true){
+                this.committed = false;
+                return true;
+            }else {
+                return false;
+            }
+
+
         }
     }
     public static class ClickSource implements SourceFunction<Event> {
@@ -75,16 +106,31 @@ public class CheckpointDemo2 {
         private String[] userArr = {"张三","李四","王五","赵六","钱七"};
         private String[] urlArr = {"www.baidu.com","csdn.net","google.com","zhihu.com"};
         private Random random = new Random();
+        private int totalCount = 0;
+
+        public ClickSource(int totalCount) {
+            if (totalCount == -1){
+                this.totalCount = Integer.MAX_VALUE;
+            }else{
+                this.totalCount = totalCount;
+            }
+
+        }
 
         @Override
         public void run(SourceContext<Event> sourceContext) throws Exception {
+            int count = 0;
             while (running){
+
                 sourceContext.collect(new Event(
                         userArr[random.nextInt(userArr.length)],
                         urlArr[random.nextInt(urlArr.length)],
                         Calendar.getInstance().getTimeInMillis()
                 ));
-                Thread.sleep(1000L);
+                count++;
+
+                Thread.sleep(500L);
+
             }
 
         }
